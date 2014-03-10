9c482df32403abaab61cdf8e81dc06ded6568edf
Dong Wang <dong77@gmail.com>
2014-03-10 14:03:01 +0800

    coinex: add demo-client

--- coinex/src/main/scala/com/coinport/coinex/DemoClient.scala
+++ coinex/src/main/scala/com/coinport/coinex/DemoClient.scala
@@ -8,22 +8,21 @@ package com.coinport.coinex
 import com.typesafe.config.ConfigFactory
 import com.coinport.coinex.domain._
 import akka.actor._
-import akka.cluster._
 import akka.cluster.routing._
 import akka.routing._
 import akka.contrib.pattern._
 import akka.persistence.Persistent
 import Implicits._
 import Currency._
+import akka.cluster.Cluster
 
-object CoinexApp extends App {
-  val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + args(0))
-    .withFallback(ConfigFactory.load())
+object DemoClient extends App {
+  val config = ConfigFactory.load("demo-client.conf")
 
   implicit val system = ActorSystem("coinex", config)
   val cluster = Cluster(system)
-
   // ------------------------------------------------------------------------------------------------
+  // Cluster-aware Routers Deployment
   // Cluster-aware Routers Deployment
   val accountProcessor = system.actorOf(
     ClusterRouterGroup(
@@ -66,40 +65,4 @@ object CoinexApp extends App {
           allowLocalRoutees = true,
           useRole = None)).props, "mv_" + market.asString + "_router")
   }
-
-  // ------------------------------------------------------------------------------------------------
-  // Processors and Views Deployment
-
-  // Account Processor
-  system.actorOf(ClusterSingletonManager.props(
-    singletonProps = Props(new AccountProcessor(marketProcessors)),
-    singletonName = "singleton",
-    terminationMessage = PoisonPill,
-    role = Some("ap")),
-    name = "ap")
-
-  // Account View
-  if (cluster.selfRoles.contains("av")) {
-    system.actorOf(Props(classOf[AccountView]), "av")
-  }
-
-  // Market Processors and Views
-  markets foreach { market =>
-    system.actorOf(ClusterSingletonManager.props(
-      singletonProps = Props(new MarketProcessor(market, accountProcessor.path)),
-      singletonName = "singleton",
-      terminationMessage = PoisonPill,
-      role = Some("mp_" + market.asString)),
-      name = "mp_" + market.asString)
-
-    if (cluster.selfRoles.contains("mv_" + market.asString)) {
-      system.actorOf(Props(new MarketView(market)), "mv_" + market.asString)
-    }
-  }
-
-  val sleep = config.getInt("coinex.app-snap-seconds")
-  println("sleeping for " + sleep + " seconds...")
-  Thread.sleep(sleep * 1000) // give time for event replay
-
-  println("============= Akka Node Ready =============\n\n")
 }
