b2f128fdc27bc0103cedd54b20bbd8450e685026
Dong Wang <dong77@gmail.com>
2014-03-10 16:10:07 +0800

    enabled DoDepositCash serializaton

--- coinex/src/main/scala/com/coinport/coinex/serializer/ThriftSerializer.scala
+++ coinex/src/main/scala/com/coinport/coinex/serializer/ThriftSerializer.scala
@@ -1,7 +1,6 @@
 package com.coinport.coinex.serializer
 
 import akka.serialization.Serializer
-import com.twitter.scrooge._
 import com.twitter.bijection.scrooge.BinaryScalaCodec
 import com.coinport.coinex.domain._
 import Currency._
@@ -18,18 +17,18 @@ abstract class ThriftSerializer extends Serializer {
   def identifier = 1234567
 
   // "toBinary" serializes the given object to an Array of Bytes
-  def toBinary(obj: AnyRef): Array[Byte] = {
-    val x = MarketSide(Rmb, Btc)
-    val y = BinaryScalaCodec(MarketSide)
-    
-    y(x)
+  def toBinary(obj: AnyRef): Array[Byte] = obj match {
+    case m: DoDepositCash =>
+      BinaryScalaCodec(DoDepositCash)(m)
+    case _ => throw new IllegalArgumentException("talk to wangdong")
   }
 
   // "fromBinary" deserializes the given array,
   // using the type hint (if any, see "includeManifest" above)
   // into the optionally provided classLoader.
   def fromBinary(bytes: Array[Byte],
-    clazz: Option[Class[_]]): AnyRef = {
-    null
+    clazz: Option[Class[_]]): AnyRef = clazz match {
+    case Some(c) if c == classOf[DoDepositCash]=> BinaryScalaCodec(DoDepositCash).invert(bytes)
+    case _ => throw new IllegalArgumentException("talk to wangdong")
   }
 }
