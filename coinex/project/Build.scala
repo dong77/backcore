ede0d55762af085b868ffde223e0c1bf38be91a9
Dong Wang <dong77@gmail.com>
2014-03-12 18:45:37 +0800

    cleanup Build.scala

--- coinex/project/Build.scala
+++ coinex/project/Build.scala
@@ -10,7 +10,6 @@ object CoinexBuild extends Build {
   val sharedSettings = Seq(
     organization := "com.coinport",
     version := "1.0.0-SNAPSHOT",
-
     scalaVersion := "2.10.3",
     crossScalaVersions := Seq("2.10.3"),
     scalacOptions ++= Seq("-encoding", "utf8"),
@@ -22,26 +21,7 @@ object CoinexBuild extends Build {
     resolvers ++= Seq(
       Resolver.sonatypeRepo("snapshots"),
       "Nexus Snapshots" at "http://192.168.0.105:8081/nexus/content/repositories/snapshots/" // "scct-github-repository" at "http://mtkopone.github.com/scct/maven-repo"
-      ),
-    libraryDependencies ++= Seq(
-      "com.typesafe.akka" %% "akka-remote" % akkaVersion,
-      "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
-      "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
-      "com.typesafe.akka" %% "akka-remote" % akkaVersion,
-      "com.typesafe.akka" %% "akka-contrib" % akkaVersion,
-      "com.typesafe.akka" %% "akka-persistence-experimental" % akkaVersion,
-      "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
-      "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
-      "com.twitter" %% "bijection-scrooge" % bijectionVersion,
-      "com.twitter" %% "scrooge-core" % "3.12.3",
-      "com.twitter" %% "scrooge-serializer" % "3.12.3",
-      "org.apache.thrift" % "libthrift" % "0.8.0",
-      "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.7",
-      "com.github.ddevore" %% "akka-persistence-mongo-casbah" % "0.4-SNAPSHOT",
-      "org.specs2" %% "specs2" % "2.3.8" % "test",
-      "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
-      "org.apache.commons" % "commons-lang3" % "3.1",
-      "ch.qos.logback" % "logback-classic" % "1.0.13")) //.settings( net.leifwarner.SbtGitInfo.setting:_*    )
+      ))
 
   lazy val root = Project(
     id = "coinex",
@@ -55,6 +35,12 @@ object CoinexBuild extends Build {
     settings = Project.defaultSettings ++
       sharedSettings ++
       ScroogeSBT.newSettings)
+    .settings(libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
+      "com.twitter" %% "bijection-scrooge" % bijectionVersion,
+      "com.twitter" %% "scrooge-core" % "3.12.3",
+      "com.twitter" %% "scrooge-serializer" % "3.12.3",
+      "org.apache.thrift" % "libthrift" % "0.8.0"))
 
   lazy val backend = Project(
     id = "coinex-backend",
@@ -63,5 +49,18 @@ object CoinexBuild extends Build {
       sharedSettings ++
       ScroogeSBT.newSettings ++
       sbtassembly.Plugin.assemblySettings)
+    .settings(
+      libraryDependencies ++= Seq(
+        "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+        "com.typesafe.akka" %% "akka-contrib" % akkaVersion,
+        "com.typesafe.akka" %% "akka-persistence-experimental" % akkaVersion,
+        "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+        "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+        "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.7",
+        "com.github.ddevore" %% "akka-persistence-mongo-casbah" % "0.4-SNAPSHOT",
+        "org.specs2" %% "specs2" % "2.3.8" % "test",
+        "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
+        "org.apache.commons" % "commons-lang3" % "3.1",
+        "ch.qos.logback" % "logback-classic" % "1.0.13"))
     .dependsOn(client)
 }
