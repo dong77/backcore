a03b76a09934dce173d19e5ac49139e05bb45f52
Dong Wang <dong77@gmail.com>
2014-03-18 21:24:09 +0800

    filter out ClusterHeartbeat logs

---
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/LogFilter.scala
@@ -0,0 +1,20 @@
+
+/**
+ * Copyright (C) 2014 Coinport Inc. <http://www.coinport.com>
+ *
+ */
+
+package com.coinport.coinex
+
+import ch.qos.logback.core.filter.Filter
+import ch.qos.logback.core.spi.FilterReply
+import ch.qos.logback.classic.spi.ILoggingEvent
+
+// Filterout ClusterHeartbeat messages
+class LogFilter extends Filter[ILoggingEvent] {
+
+  def decide(event: ILoggingEvent) = {
+    if (event.getLoggerName == "akka.cluster.ClusterHeartbeatSender") FilterReply.DENY
+    else FilterReply.ACCEPT
+  }
+}
