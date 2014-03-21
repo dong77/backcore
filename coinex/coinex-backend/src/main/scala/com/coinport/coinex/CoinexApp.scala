1f54819e6d968cae5e53fa3d2f21b7d7a7649eb5
Dong Wang <dong77@gmail.com>
2014-03-21 12:43:19 +0800

    fix runtime error

--- coinex/coinex-backend/src/main/scala/com/coinport/coinex/CoinexApp.scala
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/CoinexApp.scala
@@ -20,41 +20,35 @@ import akka.util.Timeout
 import java.net.InetAddress
 
 object CoinexApp extends App {
-  if (args.length < 1 || args.length > 4) {
+  if (args.length < 2 || args.length > 4) {
     val message = """please supply 1 to 4 parameters:
         required args(0): port - supply 0 to select a port randomly
-        optioanl args(1): roles - "*" for all roles, "" for empty node, and "a,b,c" for 3 roles
-        optioanl args(2): seeds - seed note seperated by comma, i.e, "127.0.0.1:25551,127.0.0.1:25552"
-        optioanl args(3): ip - the ip for current jvm, i.e, "192.168.0.100"
-
-        Examples:
-          - 0 * localhost:25551 // select a random port and start all roles and connects to localhost:25551
-          - 0 '' // select a random port and start as an empty node
+        required args(1): seeds - seed note seperated by comma, i.e, "127.0.0.1:25551,127.0.0.1:25552"
+        optioanl args(2): roles - "*" for all roles, "" for empty node, and "a,b,c" for 3 roles
+        optioanl args(3): hostname - self hostname
       """
     println(message)
     System.exit(1)
   }
 
   val ALL_ROLES = """
-    market_processor_btc_rmb,
-    market_depth_view_btc_rmb,
     user_processor,
     account_processor,
     marke_update_processor,
+    market_processor_btc_rmb,
+    market_depth_view_btc_rmb,
     user_view,
     account_view,
     user_orders_view,
     candle_data_view,
     """
 
-  val roles =
-    if (args.length < 2) ""
-    else if (args(1) == "*") ALL_ROLES
-    else args(1).split(",").map(_.stripMargin).filter(!_.isEmpty).map("\"" + _ + "\"").mkString(",")
+  val seedNodes = args(1).split(",").map(_.stripMargin).filter(!_.isEmpty).map("\"akka.tcp://coinex@" + _ + "\"").mkString(",")
 
-  val seedNodes =
+  val roles =
     if (args.length < 3) ""
-    else args(2).split(",").map(_.stripMargin).filter(!_.isEmpty).map("\"akka.tcp://coinex@" + _ + "\"").mkString(",")
+    else if (args(2) == "*") ALL_ROLES
+    else args(2).split(",").map(_.stripMargin).filter(!_.isEmpty).map("\"" + _ + "\"").mkString(",")
 
   val hostName =
     if (args.length < 4) InetAddress.getLocalHost.getHostAddress
