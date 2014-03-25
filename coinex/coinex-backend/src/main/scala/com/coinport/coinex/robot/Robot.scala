d78f43b5dc43d74bba834415d164dff536153ef5
MaChao <chao@coinport.com>
2014-03-25 15:43:19 +0800

    adds core robot logic

--- coinex/coinex-backend/src/main/scala/com/coinport/coinex/robot/Robot.scala
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/robot/Robot.scala
@@ -5,7 +5,62 @@
 
 package com.coinport.coinex.robot
 
-import com.coinport.coinex.LocalRouters
+import org.slf4s.Logging
 
-abstract class Robot(routers: LocalRouters) {
+import com.coinport.coinex.data._
+
+object State extends Enumeration {
+  val START, DONE = Value
+}
+
+abstract class Robot(robotId: Long) extends Object with Logging {
+  val rid = robotId
+
+  import State._
+  type Enum = Enumeration#Value
+  // Option[Any] is the actual action of the robot, such as DoDepositCash.
+  // This could be restrained from outter processor
+  // TODO(c): try to make RobotMetrics as T
+  type Action = (Option[RobotMetrics]) => (Option[Any], Enum)
+
+  private var states = Map.empty[Enum, Action]
+  private var statesPayload = Map.empty[Enum, Option[Any]]
+  private var currentState: Enum = START
+
+  addHandler(START) { _ => (None, firstState) }
+  addHandler(DONE) { _ => (None, DONE) }
+  inflate
+  action(None)
+
+  // invoked by outter processor
+  def action(metrics: Option[RobotMetrics] = None): Option[Any] = {
+    val (actualAction, to) = states(currentState)(metrics)
+    transitTo(to)
+    actualAction
+  }
+
+  def isDone = currentState == DONE
+
+  protected def firstState: Enum
+
+  protected def inflate: Unit
+
+  protected def addHandler(state: Enum)(handler: Action) {
+    require(!states.contains(state), "can't set multiple handlers to one state")
+    require(state != null)
+    states += (state -> handler)
+  }
+
+  protected def addPayload(state: Enum, payload: Option[Any]) {
+    statesPayload += (state -> payload)
+  }
+
+  protected def getPayload[T]() =
+    Option(statesPayload.getOrElse(currentState, None).getOrElse(null).asInstanceOf[T])
+
+  protected def setPayload(payload: Option[Any]) {
+    statesPayload += (currentState -> payload)
+  }
+
+  private def transitTo(state: Enum) = { currentState = state }
 }
---
+++ coinex/coinex-backend/src/test/scala/com/coinport/coinex/robot/RobotSpec.scala
@@ -0,0 +1,101 @@
+/**
+ * Copyright 2014 Coinport Inc. All Rights Reserved.
+ * Author: c@coinport.com (Chao Ma)
+ */
+
+package com.coinport.coinex.robot
+
+import org.specs2.mutable._
+
+import com.coinport.coinex.data._
+import com.coinport.coinex.data.Currency._
+
+class RobotSpec extends Specification {
+  "Robot" should {
+    "simple robot" in {
+      class TestRobot(rid: Long) extends Robot(rid) {
+        object TState extends Enumeration {
+          val STATE_A, STATE_B, STATE_C = Value
+        }
+
+        import State._
+        import TState._
+
+        override def firstState = STATE_A
+
+        override def inflate {
+          addHandler(STATE_A) { metrics =>
+            (Some(1), STATE_B)
+          }
+
+          addHandler(STATE_B) { metrics =>
+            (Some(2), STATE_C)
+          }
+
+          addHandler(STATE_C) { metrics =>
+            (Some(3), DONE)
+          }
+        }
+      }
+
+      val robot = new TestRobot(1)
+      robot.rid mustEqual 1
+      robot.action(None) mustEqual Some(1)
+      robot.isDone mustEqual false
+      robot.action(None) mustEqual Some(2)
+      robot.action(None) mustEqual Some(3)
+      robot.isDone mustEqual true
+      robot.action(None) mustEqual None
+      robot.isDone mustEqual true
+    }
+
+    "robot with state payload" in {
+      val metricsImpl = Some(RobotMetrics(Map(MarketSide(Btc, Rmb) -> MarketByMetrics(MarketSide(Btc, Rmb), 123))))
+      class TestRobot(rid: Long) extends Robot(rid) {
+        object TState extends Enumeration {
+          val STATE_A, STATE_B, STATE_C = Value
+        }
+
+        import State._
+        import TState._
+
+        override def firstState = STATE_A
+
+        override def inflate {
+          addPayload(STATE_A, Some(2))
+          addPayload(STATE_B, Some(2))
+
+          addHandler(STATE_A) { metrics =>
+            metrics mustEqual metricsImpl
+            val payload = getPayload[Int]()
+            setPayload(payload map { _ - 1 })
+            println("x" * 10 + payload)
+            if (payload.get != 1) {
+              (Some(0), STATE_A)
+            } else {
+              (Some(1), STATE_B)
+            }
+          }
+          addHandler(STATE_B) { metrics =>
+            metrics mustEqual None
+            (Some(2), STATE_C)
+          }
+          addHandler(STATE_C) { metrics =>
+            metrics mustEqual None
+            (Some(3), DONE)
+          }
+        }
+      }
+
+      val robot = new TestRobot(2)
+      robot.rid mustEqual 2
+      robot.action(metricsImpl) mustEqual Some(0)
+      robot.action(metricsImpl) mustEqual Some(1)
+      robot.action(None) mustEqual Some(2)
+      robot.isDone mustEqual false
+      robot.action(None) mustEqual Some(3)
+      robot.isDone mustEqual true
+      robot.action(None) mustEqual None
+    }
+  }
+}
