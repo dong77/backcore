71813a1262003b2abb74470cce74500d7a7ec984
Dong Wang <dong77@gmail.com>
2014-03-28 01:07:16 +0800

    move hmac to client

--- coinex/coinex-backend/src/main/scala/com/coinport/coinex/util/Hash.scala
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/util/Hash.scala
@@ -1,22 +1,17 @@
+/**
+ * Copyright (C) 2014 Coinport Inc. <http://www.coinport.com>
+ *
+ */
+
 package com.coinport.coinex.util
 
 import java.security.MessageDigest
-import javax.crypto.spec.SecretKeySpec
-import javax.crypto.Mac
+
 import com.google.common.io.BaseEncoding
 
 object Hash {
   def sha256Base64(str: String) = BaseEncoding.base64.encode(MessageDigest.getInstance("SHA-256").digest(str.getBytes("UTF-8")))
   def sha256Base32(str: String) = BaseEncoding.base32.encode(MessageDigest.getInstance("SHA-256").digest(str.getBytes("UTF-8")))
-
   def murmur3(str: String): Long = MurmurHash3.MurmurHash3_x64_64(str.getBytes, 100416)
   def sha256ThenMurmur3(text: String): Long = Hash.murmur3(sha256Base64(text))
-
-  def hmacSha1Base64(text: String, key: String): String = {
-    val signingKey = new SecretKeySpec(key.getBytes, "HmacSHA1");
-    val mac = Mac.getInstance("HmacSHA1");
-    mac.init(signingKey);
-    val bytes = mac.doFinal(text.getBytes)
-    BaseEncoding.base64.encode(bytes)
-  }
 }
