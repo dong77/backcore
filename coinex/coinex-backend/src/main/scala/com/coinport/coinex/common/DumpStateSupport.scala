9168716a3afb9f8cdf7af4e9b2c7d6205aa03668
Dong Wang <dong77@gmail.com>
2014-04-10 19:00:51 +0800

    log information from inside DumpStateSupport

--- coinex/coinex-backend/src/main/scala/com/coinport/coinex/common/DumpStateSupport.scala
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/common/DumpStateSupport.scala
@@ -3,14 +3,18 @@ package com.coinport.coinex.common
 import akka.actor.Actor
 import java.io.FileOutputStream
 import akka.serialization.SerializationExtension
+import akka.actor.ActorLogging
 
-private[common] trait DumpStateSupport { self: Actor =>
+private[common] trait DumpStateSupport { self: Actor with ActorLogging =>
   def dumpToFile(state: AnyRef, file: String) = {
     val out = new FileOutputStream(file)
     val serialization = SerializationExtension(context.system)
     val serializer = serialization.findSerializerFor(state)
     try {
       out.write(serializer.toBinary(state))
+      log.info("state dumped to file: " + file)
+    } catch {
+      case e: Throwable => log.error("Unable to dump state to file " + file, e)
     } finally {
       out.close()
     }
