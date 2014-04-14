4e980232a42c62f5fc40978ef314370254ef5de0
LiuChunming <chunming02@163.com>
2014-04-14 23:32:33 +0800

    add shell script for data exporting

---
+++ coinex/shell/export.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#
+# Copyright 2014 Coinport Inc. All Rights Reserved.
+
+echo ================= Coinport Data Exporter =================
+echo export $1 events
+# data path
+path=/data
+tempFile=/tmp/export_cmd.sh
+touch $tempFile
+chmod +x $tempFile
+
+# query recent snapshot
+snapshotFile=${path}/lastsnapshot_${1}
+lastSnapshot=`cat $snapshotFile`
+
+if [ ! -f "$snapshotFile" ]; then
+    lastSnapshot=0
+fi
+
+echo from last snapshot $lastSnapshot
+query="'{\"metadata.snapshotIndex\": {\$gt: $lastSnapshot}}'"
+echo mongoexport -d coinex_events -c p_${1}_metadata -f metadata.snapshotIndex -q $query --csv -o /tmp/snapshot > $tempFile
+$tempFile
+
+# for each snapshot
+for i in `sed -n '2,$p' /tmp/snapshot`; do
+    snapshot=`expr $i - 1`
+    echo current snapshot $i, prepare to export snapshot $snapshot ...
+    file=${path}/coinport_${1}_snapshot_${snapshot}.json
+
+    query="'{snapshot: $snapshot}'"
+
+    if [ ! -f "$file" ]; then
+           echo mongoexport -d coinex_events -c p_${1}_events -q $query -o $file > $tempFile
+           $tempFile
+           # remove empty files
+           if [ ! -s "$file" ]; then
+               rm $file
+           fi
+    fi
+done
+echo $snapshot > $snapshotFile
+echo last snapshot is $snapshot
+
+echo ==========================================================
