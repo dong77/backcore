7f7d854878fdae6fa8f4ea8c62fb18f75aa4d8e1
liweichao <weichao092@163.com>
2014-04-29 11:39:34 +0800

    Revert change for open_data_config

--- coinex/coinex-backend/src/main/resources/open_data_config.scala
+++ coinex/coinex-backend/src/main/resources/open_data_config.scala
@@ -1,9 +1,6 @@
-import scala.collection.mutable.Map
 import com.coinport.coinex.common.PersistentId._
 import com.coinport.coinex.data._
+import scala.collection.immutable.Map
 import Implicits._
 
-/**
- * Created by liweichao on 14-4-21.
- */
-Map((ROBOT_PROCESSOR <<) -> 0L, (MARKET_UPDATE_PROCESSOR <<) -> 0L, (METRICS_VIEW <<) -> 0L)
+Map((MARKET_UPDATE_PROCESSOR <<) -> "market", (ACCOUNT_PROCESSOR <<) -> "account", "p_m_btcrmb" -> "btc_cny")
