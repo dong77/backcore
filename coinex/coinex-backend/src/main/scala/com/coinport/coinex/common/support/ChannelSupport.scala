2192953e49305aaeb92ffebcda8a1f2cfc22d098
liweichao <weichao092@163.com>
2014-05-06 17:04:07 +0800

    Set channel redeliverInterval to 10 second

--- coinex/coinex-backend/src/main/scala/com/coinport/coinex/common/support/ChannelSupport.scala
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/common/support/ChannelSupport.scala
@@ -1,15 +1,16 @@
 package com.coinport.coinex.common.support
 
 import akka.actor.Actor
-import akka.persistence.Channel
+import akka.persistence.{ Channel, ChannelSettings}
 import akka.persistence.ConfirmablePersistent
+import scala.concurrent.duration._
 
 trait ChannelSupport { self: Actor =>
   def processorId: String
 
   protected def createChannelTo(dest: String) = {
     val channelName = processorId + "_2_" + dest
-    context.actorOf(Channel.props(channelName), channelName)
+    context.actorOf(Channel.props(channelName, ChannelSettings(redeliverInterval = 10 seconds)), channelName)
   }
 
   def confirm(p: ConfirmablePersistent): Unit
