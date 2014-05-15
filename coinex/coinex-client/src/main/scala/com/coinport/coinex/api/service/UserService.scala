39fc7d5b51cfcfebad676c0e6eb892fabfff9e44
Kongliang Zhong <zhongkl@gmail.com>
2014-05-15 16:22:25 +0800

    bug fix for login: nullpointer exception on register

--- coinex/coinex-client/src/main/scala/com/coinport/coinex/api/service/UserService.scala
+++ coinex/coinex-client/src/main/scala/com/coinport/coinex/api/service/UserService.scala
@@ -33,8 +33,8 @@ object UserService extends AkkaService {
       loginToken = None,
       googleAuthenticatorSecret = None,
       status = UserStatus.Normal,
-      depositAddresses = null,
-      withdrawalAddresses = null
+      depositAddresses = None,
+      withdrawalAddresses = None
     )
 
     val command = DoRegisterUser(profile, password)
