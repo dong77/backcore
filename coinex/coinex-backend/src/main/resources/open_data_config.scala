2616ee987e7c66ceecfcd09165c4023aedd7ee09
liweichao <weichao092@163.com>
2014-05-24 16:22:13 +0800

    revert open_data_config, set default to false

--- coinex/coinex-backend/src/main/resources/open_data_config.scala
+++ coinex/coinex-backend/src/main/resources/open_data_config.scala
@@ -7,7 +7,7 @@ import com.coinport.coinex.serializers._
 import Implicits._
 
 new OpenDataConfig {
-  override val enableExportData = true
+  override val enableExportData = false
   override val pFileMap = scala.collection.mutable.Map((MARKET_UPDATE_PROCESSOR <<) -> "market", (ACCOUNT_PROCESSOR <<) -> "account", (ACCOUNT_TRANSFER_PROCESSOR <<) -> "deposit_withdrawal")
   com.coinport.coinex.CoinexApp.markets foreach { marketSide =>
     pFileMap += (MARKET_PROCESSOR << marketSide) -> ("market_" + marketSide.s)
@@ -17,6 +17,6 @@ new OpenDataConfig {
   override val exportMessagesHdfsDir = "/export/messages/"
   override val debugSnapshotHdfsDir = "/debug/snapshot"
   override val hdfsHost = "hdfs://hadoop:54310"
-  override val scheduleInterval = 30 second
+  override val scheduleInterval = 900 second
   override val snapshotSerializerMap = Map(TAccountTransferState.Immutable.getClass.getEnclosingClass.getSimpleName -> DebugJsonSerializer)
 }
