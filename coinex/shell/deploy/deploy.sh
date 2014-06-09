c1bc96beeb9dd011a9ae32152e1a390284ab3575
mmmmmagina <jaice_229@163.com>
2014-06-09 17:23:04 +0800

    fix deploy script

--- coinex/shell/deploy/deploy.sh
+++ coinex/shell/deploy/deploy.sh
@@ -1,2 +1,2 @@
 ssh -i ~/work/xiaolu.pem ubuntu@54.238.180.101 "/home/ubuntu/coinport/backcore/coinex/shell/deploy/generateTar.sh $1"
-ssh -i ~/work/xiaolu.pem ubuntu@54.238.180.101 'cd /home/ubuntu/coinport/backcore/coinex/coinex-backend/target/scala-2.10 && ../../../shell/deploy/start-coinex-backend.sh'
+ssh -i ~/work/xiaolu.pem ubuntu@54.238.180.101 'cd /home/ubuntu/coinport/coinex-backend && ../backcore/coinex/shell/deploy/start-coinex-backend.sh'
