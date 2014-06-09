ae7d17242d82c66eb890b7c8c49afcba4f38604a
mmmmmagina <jaice_229@163.com>
2014-06-09 16:28:56 +0800

    fix deploy script

--- coinex/shell/deploy/generateTar.sh
+++ coinex/shell/deploy/generateTar.sh
@@ -1,6 +1,7 @@
 cd ~/coinport/backcore/coinex
 git fetch
 git checkout -b $1 remotes/origin/$1
+git rebase origin/$1
 ./activator clean
 ./activator assembly
 cp ~/coinport/backcore/coinex/coinex-backend/target/scala-2.10/coinex-backend-assembly-* ~/coinport/coinex-backend/
