6b374cc2bf841965cc9bc48732f9d5a2a616400e
Kongliang Zhong <zhongkl@gmail.com>
2014-06-13 20:44:52 +0800

    update model class User

--- coinex/coinex-client/src/main/scala/com/coinport/coinex/api/model/User.scala
+++ coinex/coinex-client/src/main/scala/com/coinport/coinex/api/model/User.scala
@@ -5,6 +5,7 @@
 package com.coinport.coinex.api.model
 
 import com.coinport.coinex.data.Currency
+import com.coinport.coinex.data.UserStatus
 
 case class User(id: Long,
   email: String,
@@ -13,4 +14,5 @@ case class User(id: Long,
   nationalId: Option[String] = None,
   mobile: Option[String] = None,
   depositAddress: Option[Map[Currency, String]] = None,
-  withdrawalAddress: Option[Map[Currency, String]] = None)
+  withdrawalAddress: Option[Map[Currency, String]] = None,
+  status: UserStatus = UserStatus.Normal)
