4e5afd1dc0d191ffa9b0b320dd32a91a85cbfa55
liweichao <weichao092@163.com>
2014-06-18 16:31:22 +0800

    modify test resource config file

--- coinex/coinex-backend/src/test/resources/integration_open_data_config.scala
+++ coinex/coinex-backend/src/test/resources/integration_open_data_config.scala
@@ -1,6 +1,22 @@
 import com.coinport.coinex.common.PersistentId._
 import com.coinport.coinex.data._
-import scala.collection.Set
+import com.coinport.coinex.opendata._
+import scala.concurrent.duration._
+
 import Implicits._
 
-Set(MARKET_UPDATE_PROCESSOR <<, METRICS_VIEW <<, "p_m_btcrmb")
+new OpenDataConfig {
+  override val enableExportData = false
+  override val pFileMap = scala.collection.mutable.Map((MARKET_UPDATE_PROCESSOR <<) -> "market", (ACCOUNT_PROCESSOR <<) -> "account", (ACCOUNT_TRANSFER_PROCESSOR <<) -> "deposit_withdrawal")
+  com.coinport.coinex.CoinexApp.markets foreach { marketSide =>
+    pFileMap += (MARKET_PROCESSOR << marketSide) -> ("market_" + marketSide.s)
+  }
+  override val snapshotHdfsDir = "/snapshot/"
+  override val exportSnapshotHdfsDir = "/export/snapshot/"
+  override val exportMessagesHdfsDir = "/export/messages/"
+  override val debugSnapshotHdfsDir = "/debug/snapshot"
+  override val hdfsHost = "hdfs://hadoop:54310"
+  override val scheduleInterval = 900 second
+  override val openSnapshotSerializerMap = Map.empty[String,com.coinport.coinex.serializers.BaseJsonSerializer]
+  override val openSnapshotFilterMap = Map(TAccountState.Immutable.getClass.getEnclosingClass.getSimpleName -> TAccountStateFilter)
+}
