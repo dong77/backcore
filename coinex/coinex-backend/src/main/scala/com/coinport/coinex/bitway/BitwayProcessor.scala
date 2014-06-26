94c2dd4c101834c865080450f8780d87ffe78dc0
MaChao <chao@coinport.com>
2014-06-26 22:07:14 +0800

    fix the null pointer exception when log in BitwayProcessor

--- coinex/coinex-backend/src/main/scala/com/coinport/coinex/bitway/BitwayProcessor.scala
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/bitway/BitwayProcessor.scala
@@ -165,7 +165,7 @@ class BitwayProcessor(transferProcessor: ActorRef, supportedCurrency: Currency, 
       }
     case m @ BitwayMessage(currency, None, None, Some(blockMsg), None, None) =>
       val continuity = manager.getBlockContinuity(blockMsg)
-      log.info("receive new block: " + blockMsg.block.index + " which recognizied as " + continuity)
+      log.info("receive new block: " + blockMsg + " which recognizied as " + continuity)
       log.info("maintained index list is: " + manager.getBlockIndexes)
       continuity match {
         case DUP => log.info("receive block which has been seen: " + blockMsg.block.index)
