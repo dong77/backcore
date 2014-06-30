614e9d1847e2f97ff78e96daea28438d2b6462ca
liweichao <weichao092@163.com>
2014-06-30 13:33:53 +0800

    decrease confirmNum for test config

--- coinex/coinex-backend/src/main/resources/account_transfer-test.scala
+++ coinex/coinex-backend/src/main/resources/account_transfer-test.scala
@@ -4,6 +4,6 @@ import com.coinport.coinex.data.Currency._
 
 new AccountTransferConfig {
   override val transferDebug = false
-  override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 2, Ltc -> 3, Doge -> 4)
+  override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 1, Ltc -> 1, Doge -> 1)
   override val succeededRetainNum: collection.Map[Currency, Int] = collection.Map(Btc -> 100, Ltc -> 200, Doge -> 300)
 }
