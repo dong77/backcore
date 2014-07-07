b30debf27cf4cf06835b211caf70f14c04f06955
liweichao <weichao092@163.com>
2014-07-07 20:07:48 +0800

    Reconfig transfer's confirm num

--- coinex/coinex-backend/src/main/resources/account_transfer-prod.scala
+++ coinex/coinex-backend/src/main/resources/account_transfer-prod.scala
@@ -4,6 +4,6 @@ import com.coinport.coinex.data.Currency._
 
 new AccountTransferConfig {
   override val transferDebug = false
-  override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 2, Ltc -> 3, Doge -> 4)
+  override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 2, Ltc -> 4, Doge -> 4, Bc -> 4, Drk -> 4)
   override val succeededRetainNum: collection.Map[Currency, Int] = collection.Map(Btc -> 100, Ltc -> 200, Doge -> 300)
 }
