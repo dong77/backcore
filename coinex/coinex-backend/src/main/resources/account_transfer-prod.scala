cd08b3a49321325ed9af4a715f979418c326e241
liweichao <weichao092@163.com>
2014-07-22 19:42:03 +0800

    remove manualCurrency for btsx

--- coinex/coinex-backend/src/main/resources/account_transfer-prod.scala
+++ coinex/coinex-backend/src/main/resources/account_transfer-prod.scala
@@ -5,6 +5,5 @@ import com.coinport.coinex.data.Currency._
 new AccountTransferConfig {
   override val transferDebug = false
   override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 1, Ltc -> 4, Doge -> 4, Bc -> 10, Drk -> 4, Vrc -> 4, Zet -> 10)
-  override val manualCurrency = collection.Set[Currency](Btsx)
   override val succeededRetainNum: collection.Map[Currency, Int] = collection.Map(Btc -> 100, Ltc -> 200, Doge -> 300)
 }
