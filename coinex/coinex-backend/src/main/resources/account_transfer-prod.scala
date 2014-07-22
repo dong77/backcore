0f6ed598b47cde4b0e12f6d59e8c06dbdbd8945c
liweichao <weichao092@163.com>
2014-07-22 19:52:04 +0800

    add account transfer config for btsx

--- coinex/coinex-backend/src/main/resources/account_transfer-prod.scala
+++ coinex/coinex-backend/src/main/resources/account_transfer-prod.scala
@@ -4,6 +4,6 @@ import com.coinport.coinex.data.Currency._
 
 new AccountTransferConfig {
   override val transferDebug = false
-  override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 1, Ltc -> 4, Doge -> 4, Bc -> 10, Drk -> 4, Vrc -> 4, Zet -> 10)
+  override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 1, Ltc -> 4, Doge -> 4, Bc -> 10, Drk -> 4, Vrc -> 4, Zet -> 10, Btsx -> 10)
   override val succeededRetainNum: collection.Map[Currency, Int] = collection.Map(Btc -> 100, Ltc -> 200, Doge -> 300)
 }
--- coinex/coinex-backend/src/main/resources/account_transfer.scala
+++ coinex/coinex-backend/src/main/resources/account_transfer.scala
@@ -4,6 +4,6 @@ import com.coinport.coinex.data.Currency._
 
 new AccountTransferConfig {
   override val transferDebug = true
-  override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 1, Ltc -> 1, Doge -> 1, Bc -> 1, Drk -> 1, Vrc -> 1, Zet -> 1)
+  override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 1, Ltc -> 1, Doge -> 1, Bc -> 1, Drk -> 1, Vrc -> 1, Zet -> 1, Btsx -> 1)
   override val succeededRetainNum: collection.Map[Currency, Int] = collection.Map(Btc -> 100, Ltc -> 200, Doge -> 300)
 }
