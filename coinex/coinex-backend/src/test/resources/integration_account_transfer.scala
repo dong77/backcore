82e6a354b80107cbe1ff2255d5346450bbeec5b1
weichao <weichao@coinport.com>
2014-08-18 13:16:20 +0800

    Fix TransferBehaviorSpec bug

--- coinex/coinex-backend/src/test/resources/integration_account_transfer.scala
+++ coinex/coinex-backend/src/test/resources/integration_account_transfer.scala
@@ -6,4 +6,15 @@ new AccountTransferConfig {
   override val transferDebug = true
   override val confirmNumMap: collection.Map[Currency, Int] = collection.Map(Btc -> 2, Ltc -> 2, Doge -> 2)
   override val succeededRetainNum: collection.Map[Currency, Int] = collection.Map(Btc -> 100, Ltc -> 200, Doge -> 300)
+  override val autoConfirmAmount: collection.Map[Currency, Long] =
+    collection.Map(
+      Btc -> 3E7.toLong,
+      Ltc -> 10E8.toLong,
+      Doge -> 10E8.toLong,
+      Bc -> 300E8.toLong,
+      Drk -> 10E8.toLong,
+      Vrc -> 20E8.toLong,
+      Zet -> 1000E8.toLong,
+      Btsx -> 20000E8.toLong,
+      Nxt -> 100E8.toLong)
 }
--- coinex/coinex-backend/src/test/scala/com/coinport/coinex/transfer/TransferBehaviorSpec.scala
+++ coinex/coinex-backend/src/test/scala/com/coinport/coinex/transfer/TransferBehaviorSpec.scala
@@ -31,9 +31,10 @@ class TransferBehaviorSpec extends IntegrationSpec(new Environment) {
       dw.batchAccountMessage(currency).size should equal(0)
       dw.batchBitwayMessage(currency).size should equal(0)
     }
-    val transferConfig = new AccountTransferConfig(false, Map.empty, Set.empty, Map.empty)
-    dw.setSucceededRetainNum(transferConfig.confirmNumMap)
+    val accountTransferConfig = new AccountTransferConfig(false, Map.empty, Set.empty, Map.empty)
+    dw.setSucceededRetainNum(accountTransferConfig.confirmNumMap)
     dw.intTransferHandlerObjectMap()
+    val transferConfig = TransferConfig(manualCurrency = Some(accountTransferConfig.manualCurrency), enableAutoConfirm = Some(accountTransferConfig.enableAutoConfirm))
 
     "be able to save transferHandler and query them" in {
       val d1 = AccountTransfer(id = 1, userId = 1, `type` = TransferType.Deposit, currency = Currency.Cny, amount = 1000, created = Some(100), updated = Some(800))
@@ -73,31 +74,31 @@ class TransferBehaviorSpec extends IntegrationSpec(new Environment) {
 
       val q = QueryTransfer(uid = Some(1000000000), currency = Some(Btc), cur = Cursor(0, 10))
 
-      val deposit = DoRequestTransfer(AccountTransfer(101, 1000000000, Deposit, Btc, 100))
+      val deposit = DoRequestTransfer(AccountTransfer(101, 1000000000, Deposit, Btc, 100, created = Some(System.currentTimeMillis())), None, Some(transferConfig))
       dw.updateState(deposit)
       dw.transferHandler.find(dw.transferHandler.getQueryDBObject(q.copy(types = List(Deposit))), 0, 10).size should equal(0)
       dw.transferHandlerObjectMap(Deposit).id2HandlerMap.isEmpty should be(true)
       resListIsEmpty(Btc)
 
-      val userToHot = DoRequestTransfer(AccountTransfer(102, 1000000000, UserToHot, Btc, 10, address = Some("userAddress")))
+      val userToHot = DoRequestTransfer(AccountTransfer(102, 1000000000, UserToHot, Btc, 10, address = Some("userAddress"), created = Some(System.currentTimeMillis())), None, Some(transferConfig))
       dw.updateState(userToHot)
       dw.transferHandler.find(dw.transferHandler.getQueryDBObject(q.copy(types = List(UserToHot))), 0, 10).size should equal(0)
       dw.transferHandlerObjectMap(UserToHot).id2HandlerMap.isEmpty should be(true)
       resListIsEmpty(Btc)
 
-      val withdrawal = DoRequestTransfer(AccountTransfer(103, 1000000000, Withdrawal, Btc, 10))
+      val withdrawal = DoRequestTransfer(AccountTransfer(103, 1000000000, Withdrawal, Btc, 10, created = Some(System.currentTimeMillis())), None, Some(transferConfig))
       dw.updateState(withdrawal)
       dw.transferHandler.find(dw.transferHandler.getQueryDBObject(q.copy(types = List(Withdrawal))), 0, 10).map(_.id) should equal(Seq(103))
       dw.transferHandlerObjectMap(Withdrawal).id2HandlerMap.isEmpty should be(true)
       resListIsEmpty(Btc)
 
-      val coldToHot = DoRequestTransfer(AccountTransfer(104, 1000000000, ColdToHot, Btc, 10))
+      val coldToHot = DoRequestTransfer(AccountTransfer(104, 1000000000, ColdToHot, Btc, 10, created = Some(System.currentTimeMillis())), None, Some(transferConfig))
       dw.updateState(coldToHot)
       dw.transferHandler.find(dw.transferHandler.getQueryDBObject(q.copy(types = List(ColdToHot))), 0, 10).map(_.id) should equal(Seq(104))
       dw.transferHandlerObjectMap(ColdToHot).id2HandlerMap.isEmpty should be(true)
       resListIsEmpty(Btc)
 
-      val hotToCold = DoRequestTransfer(AccountTransfer(105, 1000000000, HotToCold, Btc, 10))
+      val hotToCold = DoRequestTransfer(AccountTransfer(105, 1000000000, HotToCold, Btc, 10, created = Some(System.currentTimeMillis())), None, Some(transferConfig))
       dw.updateState(hotToCold)
       dw.transferHandler.find(dw.transferHandler.getQueryDBObject(q.copy(types = List(HotToCold))), 0, 10).map(_.id) should equal(Seq(105))
       dw.transferHandlerObjectMap(HotToCold).id2HandlerMap.size should be(1)
@@ -108,7 +109,7 @@ class TransferBehaviorSpec extends IntegrationSpec(new Environment) {
       h2CItem.id should equal(itemId)
       h2CItem.txType.get should equal(HotToCold)
 
-      val unknown = DoRequestTransfer(AccountTransfer(106, 1000000000, TransferType.Unknown, Btc, 10))
+      val unknown = DoRequestTransfer(AccountTransfer(106, 1000000000, TransferType.Unknown, Btc, 10, created = Some(System.currentTimeMillis())), None, Some(transferConfig))
       dw.updateState(unknown)
       dw.transferHandler.find(dw.transferHandler.getQueryDBObject(q.copy(types = List(TransferType.Unknown))), 0, 10).map(_.id) should equal(Seq(106))
 
