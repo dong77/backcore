bf68a5c44998cb8ad1425fdb84298b3bd797f15b
weichao <weichao@coinport.com>
2014-08-18 12:05:03 +0800

    config confirm batch size to small

--- coinex/coinex-backend/src/main/resources/open_data_config-test.scala
+++ coinex/coinex-backend/src/main/resources/open_data_config-test.scala
@@ -7,7 +7,7 @@ import Implicits._
 
 new OpenDataConfig {
   override val enableExportData = true
-  override val pFileMap = scala.collection.mutable.Map((MARKET_UPDATE_PROCESSOR <<) -> "market", (ACCOUNT_PROCESSOR <<) -> "account", (ACCOUNT_TRANSFER_PROCESSOR <<) -> "deposit_withdrawal")
+  override val pFileMap = scala.collection.mutable.Map((ACCOUNT_PROCESSOR <<) -> "account", (ACCOUNT_TRANSFER_PROCESSOR <<) -> "deposit_withdrawal")
   com.coinport.coinex.CoinexApp.markets foreach { marketSide =>
     pFileMap += (MARKET_PROCESSOR << marketSide) -> ("market_" + marketSide.s)
   }
