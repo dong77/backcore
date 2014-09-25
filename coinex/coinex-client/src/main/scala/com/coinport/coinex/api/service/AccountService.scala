1f303e02eac32ac05a2d588594cd2b1218cfe5e1
Kongliang Zhong <zhongkl@gmail.com>
2014-09-25 18:06:15 +0800

    add nxt public key

--- coinex/coinex-client/src/main/scala/com/coinport/coinex/api/service/AccountService.scala
+++ coinex/coinex-client/src/main/scala/com/coinport/coinex/api/service/AccountService.scala
@@ -32,10 +32,11 @@ object AccountService extends AkkaService {
     }
   }
 
-  def withdrawal(uid: Long, currency: Currency, amount: Double, address: String, memo: String): Future[ApiResult] = {
+  def withdrawal(uid: Long, currency: Currency, amount: Double, address: String, memo: String, publicKey: String): Future[ApiResult] = {
     val internalAmount: Long = amount.internalValue(currency)
 
-    val withdrawal = AccountTransfer(0L, uid.toLong, TransferType.Withdrawal, currency, internalAmount, TransferStatus.Pending, address = Some(address), memo = Some(memo))
+    val withdrawal = AccountTransfer(0L, uid.toLong, TransferType.Withdrawal, currency, internalAmount,
+      TransferStatus.Pending, address = Some(address), memo = Some(memo), nxtPublicKey = Some(publicKey))
     backend ? DoRequestTransfer(withdrawal) map {
       case result: RequestTransferSucceeded =>
         ApiResult(true, 0, "提现申请已提交", Some(result))
