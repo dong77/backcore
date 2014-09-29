93d52291da960580333e6983557aff8cd5b99fae
weichao <weichao@coinport.com>
2014-09-29 20:31:28 +0800

    Log info for userProcessor

--- coinex/coinex-backend/src/main/scala/com/coinport/coinex/users/UserProcessor.scala
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/users/UserProcessor.scala
@@ -6,9 +6,7 @@
 package com.coinport.coinex.users
 
 import akka.actor._
-import akka.actor.Actor.Receive
 import akka.event.LoggingReceive
-import akka.persistence.SnapshotOffer
 import akka.persistence._
 import com.coinport.coinex.util._
 import com.coinport.coinex.common.ExtendedProcessor
@@ -208,12 +206,12 @@ class UserProcessor(mailer: ActorRef, bitwayProcessors: collection.immutable.Map
   }
 
   private def sendVerificationCodeEmail(email: String, code: String) {
-    println(s"(email verification code : $email, $code)")
+    log.info(s"(email verification code : $email, $code)")
     mailer ! DoSendEmail(email, EmailType.VerificationCode, Map("CODE" -> code))
   }
 
   private def sendEmailVerificationEmail(profile: UserProfile) {
-    println(s"(register verification code : ${profile.email}, ${profile.verificationToken.get})")
+    log.info(s"(register verification code : ${profile.email}, ${profile.verificationToken.get})")
     mailer ! DoSendEmail(profile.email, EmailType.RegisterVerify, Map(
       "NAME" -> profile.realName.getOrElse(profile.email),
       "LANG" -> "CHINESE",
