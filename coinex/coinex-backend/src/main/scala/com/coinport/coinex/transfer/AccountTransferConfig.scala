84788493452eb57e2b281cc3eaed930a4c6a468a
weichao <weichao@coinport.com>
2014-09-30 11:55:23 +0800

    Fix bug for TransferConfig init

--- coinex/coinex-backend/src/main/scala/com/coinport/coinex/transfer/AccountTransferConfig.scala
+++ coinex/coinex-backend/src/main/scala/com/coinport/coinex/transfer/AccountTransferConfig.scala
@@ -10,5 +10,5 @@ class AccountTransferConfig(
   val succeededRetainNum: collection.Map[Currency, Int] = collection.Map(Btc -> 100, Ltc -> 200, Doge -> 300),
   val enableAutoConfirm: Boolean = true,
   val autoConfirmAmount: collection.Map[Currency, Long] = collection.Map.empty[Currency, Long],
-  val enableUsersToInner: collection.Map[Currency, Boolean] = collection.Map.empty(Btc -> true, Ltc -> true, Nxt -> false))
+  val enableUsersToInner: collection.Map[Currency, Boolean] = collection.Map(Btc -> true, Ltc -> true, Nxt -> false))
 
