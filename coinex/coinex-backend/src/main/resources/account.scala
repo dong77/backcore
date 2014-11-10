6190788136de352978b748b1adf9e7134d8547f4
MaChao <chao@coinport.com>
2014-11-10 10:56:45 +0800

    changes the cny withdrawal fee from 0.005 to 0.004

--- coinex/coinex-backend/src/main/resources/account.scala
+++ coinex/coinex-backend/src/main/resources/account.scala
@@ -62,7 +62,7 @@ AccountConfig(
       Btsx -> ConstantFee(2.internalValue(Btsx)),
       Nxt -> ConstantFee(2.internalValue(Nxt)),
       Xrp -> ConstantFee(1.internalValue(Xrp)),
-      Cny -> PercentageFee(0.005)),
+      Cny -> PercentageFee(0.004)),
 
     freeOfTxChargeUserIdThreshold = 1E9.toLong + 2000 // 1 thousand
     )
